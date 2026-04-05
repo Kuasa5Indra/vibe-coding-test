0106305bab6dc2f6e061badf53e742263be65242
Kuasa5Indra <adityaindra135@gmail.com>
2026-04-05 21:29:29 +0700

    Fix 500 Internal Server Error when input exceeds 255 chars

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -15,9 +15,9 @@ export const usersRoute = new Elysia({ prefix: '/api/users' })
         return result;
     }, {
         body: t.Object({
-            name: t.String(),
-            email: t.String({ format: 'email' }),
-            password: t.String()
+            name: t.String({ maxLength: 255 }),
+            email: t.String({ format: 'email', maxLength: 255 }),
+            password: t.String({ maxLength: 255 })
         })
     })
     .post('/login', async ({ body, set }) => {
@@ -31,8 +31,8 @@ export const usersRoute = new Elysia({ prefix: '/api/users' })
         return result;
     }, {
         body: t.Object({
-            email: t.String({ format: 'email' }),
-            password: t.String()
+            email: t.String({ format: 'email', maxLength: 255 }),
+            password: t.String({ maxLength: 255 })
         })
     })
     .get('/current', async ({ token, set }) => {
